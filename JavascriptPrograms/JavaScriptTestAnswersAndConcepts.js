566b3ce32b7ad9d650554bed0a114847c05dc79b
abhi-trumpf <Abhishek.Narayanasami@trumpf.com>
2026-05-19 14:51:52 +0530

    for reference

---
+++ JavascriptPrograms/JavaScriptTestAnswersAndConcepts.js
@@ -0,0 +1,24 @@
+//console.log("bye");
+
+//The FIZZBUZZ
+/*
+let n =50;
+for(let i =1;i<=n;i++){
+  if(i%3==0 && i%5==0){   
+    console.log("FizzBuzz");
+  }
+  else if(i%3==0){
+    console.log("Fizz");
+  }
+
+  else if(i%5==0)
+  {
+    console.log("Buzz");
+  }
+
+  else{
+    console.log(i);
+  }
+}
+*/
+
